a47acfa2a391177ec8e5fb36818f14a92f9da999
xandowski <xandy-moraes@hotmail.com>
2022-06-08 23:23:40 -0300

    finish app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Header } from "./Components/Header/Index";
+import { Sidebar } from "./Components/Sidebar/Index";
+
 import './global.css'
+import styles from './App.module.css'
+import { Post } from "./Components/Post/Index";
 
 export function App() {
   return (
     <>
       <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post/>
+          <Post/>
+        </main>
+      </div>
     </>
   )
 }
