fe239f54f2d85fc6abc4806019d54e43e7d0fa5d
Xandowski <xandy-moraes@hotmail.com>
2022-07-12 22:39:56 -0300

    feat: Create Header

--- src/Components/Header/Index.tsx
+++ src/Components/Header/Index.tsx
@@ -1,11 +1,22 @@
 import styles from './Header.module.css'
 import Logo from '../../assets/Logo.svg'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export function Header() {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
   return (
     <header className={styles.header}>
-      <img src={Logo} alt="logo do ignite"/>
-      <span>Ignite Feed</span>
+      <div>
+        <img src={Logo} alt="logo do ignite" />
+        <span>Ignite Feed</span>
+      </div>
+
+      {isAuthenticated ? (
+        <button onClick={() => logout({ returnTo: window.location.origin })}>Sair</button>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Entrar</button>
+      )}
+
     </header>
   )
 }
